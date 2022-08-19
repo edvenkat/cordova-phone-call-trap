7cdbc7e2ccbe329d00d8fe75027aae29fc29cf88
Venka Tesh G <gvenkatesh225@gmail.com>
2022-08-19 17:41:12 +0530

    Update PhoneCallTrap.java

--- src/android/PhoneCallTrap.java
+++ src/android/PhoneCallTrap.java
@@ -41,7 +41,7 @@ class CallStateListener extends PhoneStateListener {
         this.callbackContext = callbackContext;
     }
 
-    public void onCallStateChanged(int state, String incomingNumber) {
+    public void onCallStateChanged(int state, String incomingNumber, Intent intent) {
         super.onCallStateChanged(state, incomingNumber);
 
         if (callbackContext == null) return;
@@ -58,7 +58,7 @@ class CallStateListener extends PhoneStateListener {
             break;
 
             case TelephonyManager.CALL_STATE_RINGING:
-                 Bundle bundle = Intent intent.getExtras();
+                 Bundle bundle = intent.getExtras();
       String phoneNumber = bundle.getString("incoming_number");
              msg = "RINGING&" + phoneNumber;
             //msg = "RINGING";
