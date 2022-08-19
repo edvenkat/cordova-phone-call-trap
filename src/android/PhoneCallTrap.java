c8dd19de56609a1276aac8db77487326269270b1
Venka Tesh G <gvenkatesh225@gmail.com>
2022-08-19 16:46:30 +0530

    Update PhoneCallTrap.java

--- src/android/PhoneCallTrap.java
+++ src/android/PhoneCallTrap.java
@@ -7,6 +7,7 @@ import android.content.Context;
 import android.telephony.PhoneStateListener;
 import android.telephony.TelephonyManager;
 import android.os.Bundle;
+import android.content.Intent;
 import org.json.JSONException;
 import org.json.JSONArray;
 
